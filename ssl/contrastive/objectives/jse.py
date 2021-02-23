3c5b65b42a9572da326cefb2d9b8056460c90a72
zoexu119 <zhaxu@umich.edu>
2021-02-23 01:11:23 -0600

    Add JSE_loss

--- ssl/contrastive/objectives/jse.py
+++ ssl/contrastive/objectives/jse.py
@@ -1,4 +1,22 @@
 import torch
+import torch.nn.functional as F
 
-def JSE_loss(zs):
-    pass
+
+def JSE_loss(zs, pos=True, neg=False):
+    '''
+    Args:
+        zs: List of two tensors of shape [z_dim]
+    '''
+
+    d_prime = torch.matmul(zs[0], zs[1])
+
+    if pos:
+        score = - F.softplus(-d_prime)
+
+    elif neg:
+        score = F.softplus(-d_prime) + d_prime
+    else:
+        raise Exception("Must define positive or negative pair!")
+
+    # loss = mean(neg_scores) - mean(pos_scores)
+    return score
