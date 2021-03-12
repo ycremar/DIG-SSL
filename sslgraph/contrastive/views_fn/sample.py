f03e761b4e4010ea894678460dd5a39760009842
zoexu119 <zhaxu@umich.edu>
2021-03-12 15:35:10 -0800

    Update sample adj

--- sslgraph/contrastive/views_fn/sample.py
+++ sslgraph/contrastive/views_fn/sample.py
@@ -18,8 +18,7 @@ def uniform_sample(ratio=0.1):
         idx_drop = np.random.choice(node_num, drop_num, replace=False)
         idx_nondrop = [n for n in range(node_num) if not n in idx_drop]
         adj = to_dense_adj(data.edge_index)[0]
-        adj[idx_drop, :] = 0
-        adj[:, idx_drop] = 0
+        adj = adj[idx_nondrop, :][:, idx_nondrop]
         
         return Data(x=data.x[idx_nondrop], edge_index=dense_to_sparse(adj)[0])
     
@@ -85,8 +84,7 @@ def RW_sample(ratio=0.1, add_self_loop=True):
         idx_drop = [n for n in range(node_num) if not n in idx_sub]
         idx_sampled = idx_sub
         adj = to_dense_adj(edge_index)[0]
-        adj[idx_drop, :] = 0
-        adj[:, idx_drop] = 0
+        adj = adj[idx_sampled, :][:, idx_sampled]
 
         return Data(x=data.x[idx_sampled], edge_index=dense_to_sparse(adj)[0])
 
