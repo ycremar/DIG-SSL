fe172cddb726cf853fe845a6974b4b867028c0d2
Jingtun Zhang <zjt6791@tamu.edu>
2021-03-12 02:20:30 -0600

    add resgcn to encoder

--- ssl/utils/encoders.py
+++ ssl/utils/encoders.py
@@ -1,19 +1,34 @@
 import torch
-from torch.nn import Sequential, Linear, ReLU, BatchNorm1d
+import torch.nn as nn
 import torch.nn.functional as F
+
+from functools import partial
+from torch.nn import Parameter
+from torch.nn import Sequential, Linear, ReLU, BatchNorm1d
+from torch_scatter import scatter_add
 from torch_geometric.nn import GINConv, GCNConv, global_add_pool, global_mean_pool
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import remove_self_loops, add_self_loops
+from torch_geometric.nn.inits import glorot, zeros
 
 
 class Encoder(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, n_layers=5,
                  pool='sum', gnn='gin', bn=False, act='relu',
-                 bias=True, xavier=True, node_level=False, graph_level=True):
+                 bias=True, xavier=True, node_level=False, graph_level=True,
+                 dataset=None):
         super(Encoder, self).__init__()
 
         if gnn == 'gin':
             self.encoder = GIN(input_dim, hidden_dim, n_layers, pool, bn, act)
         elif gnn == 'gcn':
             self.encoder = GCN(input_dim, hidden_dim, n_layers, pool, bn, act, bias, xavier)
+        elif gnn == 'resgcn':
+            if dataset is not None:
+                self.encoder = ResGCN(dataset, hidden_dim, num_conv_layers=n_layers, global_pool=pool)
+            else
+                raise ValueError("dataset can not be none")
+
         self.node_level = node_level
         self.graph_level = graph_level
 
@@ -135,3 +150,198 @@ class GCN(torch.nn.Module):
 
         return global_rep, x
 
+
+class ResGCNConv(MessagePassing):
+    def __init__(self, in_channels, out_channels,
+                 improved=False, cached=False, bias=True, edge_norm=True, gfn=False):
+        super(ResGCNConv, self).__init__('add')
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.improved = improved
+        self.cached = cached
+        self.cached_result = None
+        self.edge_norm = edge_norm
+        self.gfn = gfn
+
+        self.weight = Parameter(torch.Tensor(in_channels, out_channels))
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.weights_init()
+
+    def weights_init(self):
+        glorot(self.weight)
+        zeros(self.bias)
+        self.cached_result = None
+
+    @staticmethod
+    def norm(edge_index, num_nodes, edge_weight, improved=False, dtype=None):
+        if edge_weight is None:
+            edge_weight = torch.ones((edge_index.size(1), ),
+                                     dtype=dtype,
+                                     device=edge_index.device)
+        edge_weight = edge_weight.view(-1)
+        assert edge_weight.size(0) == edge_index.size(1)
+
+        edge_index, edge_weight = remove_self_loops(edge_index, edge_weight)
+        edge_index = add_self_loops(edge_index, num_nodes=num_nodes)
+        # Add edge_weight for loop edges.
+        loop_weight = torch.full((num_nodes, ),
+                                 1 if not improved else 2,
+                                 dtype=edge_weight.dtype,
+                                 device=edge_weight.device)
+        edge_weight = torch.cat([edge_weight, loop_weight], dim=0)
+
+        row, col = edge_index
+        deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+
+        return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
+
+    def forward(self, x, edge_index, edge_weight=None):
+        x = torch.matmul(x, self.weight)
+        if self.gfn:
+            return x
+
+        if not self.cached or self.cached_result is None:
+            if self.edge_norm:
+                edge_index, norm = ResGCNConv.norm(edge_index, x.size(0), edge_weight, self.improved, x.dtype)
+            else:
+                norm = None
+            self.cached_result = edge_index, norm
+
+        edge_index, norm = self.cached_result
+        return self.propagate(edge_index, x=x, norm=norm)
+
+    def message(self, x_j, norm):
+        if self.edge_norm:
+            return norm.view(-1, 1) * x_j
+        else:
+            return x_j
+
+    def update(self, aggr_out):
+        if self.bias is not None:
+            aggr_out = aggr_out + self.bias
+        return aggr_out
+
+    def __repr__(self):
+        return '{}({}, {})'.format(self.__class__.__name__, self.in_channels, self.out_channels)
+
+class ResGCN(torch.nn.Module):
+    def __init__(self, dataset, hidden_dim, 
+                 num_feat_layers=1, 
+                 num_conv_layers=3,
+                 num_fc_layers=2, 
+                 gfn=False, collapse=False, residual=False,
+                 global_pool="sum", dropout=0, dge_norm=True):
+
+        super(ResGCN, self).__init__()
+        assert num_feat_layers == 1 "more feat layers are not now supported"
+        self.conv_residual = residual
+        self.fc_residual = False  # no skip-connections for fc layers.
+        self.collapse = collapse
+
+        assert "sum" in global_pool or "mean" in global_pool, global_pool
+        if "sum" in global_pool:
+            self.global_pool = global_add_pool
+        else:
+            self.global_pool = global_mean_pool
+        self.dropout = dropout
+        GConv = partial(ResGCNConv, edge_norm=edge_norm, gfn=gfn)
+
+        if "xg" in dataset[0]:  # Utilize graph level features.
+            self.use_xg = True
+            self.bn1_xg = BatchNorm1d(dataset[0].xg.size(1))
+            self.lin1_xg = Linear(dataset[0].xg.size(1), hidden_dim)
+            self.bn2_xg = BatchNorm1d(hidden_dim)
+            self.lin2_xg = Linear(hidden_dim, hidden_dim)
+        else:
+            self.use_xg = False
+
+        hidden_in = dataset.num_features
+        if collapse:
+            self.bn_feat = BatchNorm1d(hidden_in)
+            self.bns_fc = torch.nn.ModuleList()
+            self.lins = torch.nn.ModuleList()
+            if "gating" in global_pool:
+                self.gating = torch.nn.Sequential(
+                    Linear(hidden_in, hidden_in),
+                    torch.nn.ReLU(),
+                    Linear(hidden_in, 1),
+                    torch.nn.Sigmoid())
+            else:
+                self.gating = None
+            for i in range(num_fc_layers - 1):
+                self.bns_fc.append(BatchNorm1d(hidden_in))
+                self.lins.append(Linear(hidden_in, hidden_dim))
+                hidden_in = hidden_dim
+            self.lin_class = Linear(hidden_in, dataset.num_classes)
+        else:
+            self.bn_feat = BatchNorm1d(hidden_in)
+            feat_gfn = True  # set true so GCNConv is feat transform
+            self.conv_feat = ResGCNConv(hidden_in, hidden_dim, gfn=feat_gfn)
+            if "gating" in global_pool:
+                self.gating = torch.nn.Sequential(
+                    Linear(hidden_dim, hidden_dim),
+                    torch.nn.ReLU(),
+                    Linear(hidden_dim, 1),
+                    torch.nn.Sigmoid())
+            else:
+                self.gating = None
+            self.bns_conv = torch.nn.ModuleList()
+            self.convs = torch.nn.ModuleList()
+            for i in range(num_conv_layers):
+                self.bns_conv.append(BatchNorm1d(hidden_dim))
+                self.convs.append(GConv(hidden_dim, hidden_dim))
+            self.bn_hidden = BatchNorm1d(hidden_dim)
+
+            # linear head 
+            # self.bns_fc = torch.nn.ModuleList()
+            # self.lins = torch.nn.ModuleList()
+            # for i in range(num_fc_layers - 1):
+            #     self.bns_fc.append(BatchNorm1d(hidden_dim))
+            #     self.lins.append(Linear(hidden_dim, hidden_dim))
+            # self.lin_class = Linear(hidden_dim, dataset.num_classes)
+
+        # BN initialization.
+        for m in self.modules():
+            if isinstance(m, (torch.nn.BatchNorm1d)):
+                torch.nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.bias, 0.0001)
+        
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        if self.use_xg:
+            # xg is (batch_size x its feat dim)
+            xg = self.bn1_xg(data.xg)
+            xg = F.relu(self.lin1_xg(xg))
+            xg = self.bn2_xg(xg)
+            xg = F.relu(self.lin2_xg(xg))
+        else:
+            xg = None
+        
+        x = self.bn_feat(x)
+        x = F.relu(self.conv_feat(x, edge_index))
+        for i, conv in enumerate(self.convs):
+            x_ = self.bns_conv[i](x)
+            x_ = F.relu(conv(x_, edge_index))
+            x = x + x_ if self.conv_residual else x_
+        gate = 1 if self.gating is None else self.gating(x)
+        x = self.global_pool(x * gate, batch)
+        x = x if xg is None else x + xg
+        
+        # for i, lin in enumerate(self.lins):
+        #     x_ = self.bns_fc[i](x)
+        #     x_ = F.relu(lin(x_))
+        #     x = x + x_ if self.fc_residual else x_
+        # x = self.bn_hidden(x)
+        if self.dropout > 0:
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        # x = self.lin_class(x)
+        # return F.log_softmax(x, dim=-1)
+        return x
